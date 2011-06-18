b800d908b080a76c1d95a064acc59cdc20a830b1
Kevin Raison <raison@chatsubo.net>
2011-06-17 18:21:50 -0700

    config example

--- cl-freeswitch.cfg.ex
+++ cl-freeswitch.cfg.ex
@@ -4,8 +4,8 @@ port=8084
 thread-pool-size=30
 fs-host=127.0.0.1
 fs-port=8021
-fs-auth=miadmin
-pstn-gateway=mi-pstn-gw.corp.mostgifted.com,pstn-01.corp.mostgifted.com
+fs-auth=cluecon
+pstn-gateway=pstn-gw.corp.company.com
 call-queues=corp
 
 [corp]
